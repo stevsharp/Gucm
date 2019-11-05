2991681acaab3f1d2b579b7fcbb2be5b85b244b3
Spyros Ponaris <sponaris@gmail.com>
2019-11-05 13:25:01 +0200

    Identity Server

---
+++ IdentityServer/Gucm.IdentityServer/Startup.cs
@@ -0,0 +1,53 @@
+﻿using IdentityServer4.Services;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gucm.IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // DbContext
+            /* Identity Server */
+
+            services
+                .AddIdentityServer()
+                .AddDeveloperSigningCredential();
+                //.AddInMemoryApiResources(Config.GetApiResources())
+                //.AddInMemoryClients(Config.GetClients());
+
+            services
+                .AddAuthentication()
+                .AddIdentityServerAuthentication();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseIdentityServer();
+        }
+    }
+}
