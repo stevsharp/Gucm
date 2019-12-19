048caf9b5474bb42be3868cbce69eac16e77655f
Spyros <sponaris@gmail.com>
2019-12-19 20:15:19 +0200

    SignalR

---
+++ Common/Common.SignalR/BaseHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Common.SignalR
+{
+    public abstract class BaseHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public abstract Task SendMessage();
+
+    }
+}
